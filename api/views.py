2623661b3ebb788d270ed232685e856a263e80a0
whidyl <whidyldev@gmail.com>
2021-02-11 14:12:43 -0700

    csrf exept

--- api/views.py
+++ api/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
 from .utils import butcher
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def api(request):
     if request.method == 'GET':
         return JsonResponse(butcher.generate_butchered_quote())
