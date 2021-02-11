16fcfb97d71a09996c581928ee96e4cc8bec5eb1
whidyl <whidyldev@gmail.com>
2021-02-10 17:16:11 -0700

    ready for herokue

---
+++ ProcFile
@@ -0,0 +1,1 @@
+web: gunicorn quote_butcher.wsgi
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import json
-from.utils import butcher
+from .utils import butcher
 
 # Create your views here.
 
